003e9fa80bd49712a22f551060948ed7f6646156
seongyeon <ysy2565@naver.com>
2021-10-12 00:00:55 +0900

    useState and immer

--- immer-tutorial/src/App.js
+++ immer-tutorial/src/App.js
@@ -12,7 +12,7 @@ const App=()=>{
     (e) => {
       const { name, value } = e.target;
       setForm(
-        produce(form, draft=>{
+        produce(draft=>{
           draft[name] = value;
         })
       );
@@ -29,7 +29,7 @@ const App=()=>{
         username:form.username
       };
       setData(
-        produce(data, draft=>{
+        produce( draft=>{
           draft.array.push(info);
         })
       );
@@ -39,12 +39,12 @@ const App=()=>{
       });
       nextId.current+=1;
     },
-    [data, form.name, form.username]
+    [form.name, form.username]
   );
   const onRemove = useCallback(
     id => {
       setData(
-        produce(data, draft => {
+        produce(draft => {
           draft.array.splice(draft.array.findIndex(info=>info.id===id),1);
         })
       );
